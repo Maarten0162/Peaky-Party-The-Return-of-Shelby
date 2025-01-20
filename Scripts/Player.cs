ce34b314d415791e8dd30936bee1c2b8ad494c56
Herobrine <92666704+Maarten0162@users.noreply.github.com>
2025-01-20 17:33:30 +0100

    playerclass

---
+++ Scripts/Player.cs
@@ -0,0 +1,61 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+	//private vars
+	private int Attrubute;
+	private int Health;
+	private bool isAlive;
+	private bool SkipTurn;
+	private int Currency;
+	private Vector2 currPos;
+
+	//debuffs
+	private int rollAdjust;
+
+	//stats
+	private int spacedMoved;
+	private int miniGamesWon;
+	private int GainedCurr;
+	private int LostCurr;
+	private int DamageDealt;
+
+	
+	//public vars
+	public Sprite2D Skin;
+
+	public Player(Sprite2D _skin)
+	{
+		Skin = _skin;
+		Currency = 100;
+		Health = 100;
+		isAlive = true;
+		SkipTurn = false;
+	}
+	
+	public override void _PhysicsProcess(double delta)
+	{
+		if (Health <= 0)
+		{
+			isAlive = false;
+		}
+		
+	}
+
+	public int Movement()
+	{
+		return 1;
+	}	
+
+	public void Attack()
+	{
+		//
+	}
+
+	public void takeDamage()
+	{
+		//
+	}
+	
+}
