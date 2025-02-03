347dd3c52d1305aae37a55e20962e6d4324d5c2c
Herobrine <92666704+Maarten0162@users.noreply.github.com>
2025-02-03 18:12:33 +0100

    Update ButtonMash.cs

--- Scripts/Minigames/ButtonMash.cs
+++ Scripts/Minigames/ButtonMash.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 public partial class ButtonMash : Minigame
@@ -8,6 +9,7 @@ public partial class ButtonMash : Minigame
     // Renamed signal to avoid conflict with method name
     [Signal] public delegate void ButtonReleasedSignalEventHandler(CharacterBody2D player, bool RightButton);
 
+    bool GameFinished = false;
     private Random rnd = new();
     private enum currentKey
     {
@@ -18,8 +20,17 @@ public partial class ButtonMash : Minigame
     }
     private currentKey CurrentKey;
 
+    private List<currentKey> keyList = new();
+
     private List<CharacterBody2D> players = new();
     CharacterBody2D player1;
+    int ScorePL1 = 0;                           
+    CharacterBody2D player2;
+    int ScorePL2 = 0;
+    CharacterBody2D player3;
+    int ScorePL3 = 0;
+    CharacterBody2D player4;
+    int ScorePL4 = 0;
 
     public override void _Ready()
     {
@@ -28,7 +39,6 @@ public partial class ButtonMash : Minigame
 
         // Connect the signal to the method
         Connect("ButtonReleasedSignal", Callable.From((CharacterBody2D player, bool RightButton) => ButtonReleased(player, RightButton)));
-
         chooseRndButton();
         switch (CurrentKey)
         {
@@ -41,72 +51,107 @@ public partial class ButtonMash : Minigame
             case currentKey.Y:
                 break;
         }
+
     }
 
-    public override void _Process(double delta)
+    public override async void _Process(double delta)
     {
-        checkKeyInput(player1);
+        await checkKeyInput(player1);
     }
 
     private void chooseRndButton()
     {
-        int i = rnd.Next(0, 4);
-
-        switch (i)
+        currentKey newkey = currentKey.A;
+        for (int i = 0; i < 10; i++)
         {
-            case 0:
-                CurrentKey = currentKey.A;
-                break;
-            case 1:
-                CurrentKey = currentKey.B;
-                break;
-            case 2:
-                CurrentKey = currentKey.X;
-                break;
-            case 3:
-                CurrentKey = currentKey.Y;
-                break;
+            int ii = rnd.Next(0, 4);
+
+            switch (ii)
+            {
+                case 0:
+                    newkey = currentKey.A;
+                    break;
+                case 1:
+                    newkey = currentKey.B;
+                    break;
+                case 2:
+                    newkey = currentKey.X;
+                    break;
+                case 3:
+                    newkey = currentKey.Y;
+                    break;
+            }
+            keyList.Add(newkey);
+            
         }
+        GD.Print(keyList.Count);
     }
 
-    private async void checkKeyInput(CharacterBody2D player)
+    private Dictionary<CharacterBody2D, bool> lastPressedState = new();
+    private Dictionary<CharacterBody2D, bool> canPressAgain = new();
+
+    private async Task checkKeyInput(CharacterBody2D player)
     {
+        if (GameFinished) return;
+
         string inputName = "";
         string playerName = "";
-        if (player == player1)
-        {
-            playerName = "PL1_";
-        }
+
+        if (player == player1) playerName = "PL1_";
+        else if (player == player2) playerName = "PL2_";
+        else if (player == player3) playerName = "PL3_";
+        else if (player == player4) playerName = "PL4_";
 
         switch (CurrentKey)
         {
-            case currentKey.A:
-                inputName = "A";
-                break;
-            default:
-                inputName = "A";
-                break;
+            case currentKey.A: inputName = "A"; break;
+            case currentKey.B: inputName = "A"; break;
+            case currentKey.X: inputName = "A"; break;
+            case currentKey.Y: inputName = "A"; break;
         }
 
+        string correctInput = playerName + inputName;
+        bool isCorrectPressed = Input.IsActionPressed(correctInput);
 
+        // Ensure the player is in tracking dictionaries
+        if (!lastPressedState.ContainsKey(player)) lastPressedState[player] = false;
+        if (!canPressAgain.ContainsKey(player)) canPressAgain[player] = true;
 
-        string input = playerName + inputName;
-        
-        if (Input.IsAnythingPressed())
+        // **Detect correct button press**
+        if (isCorrectPressed && !lastPressedState[player] && canPressAgain[player])
         {
-            if (Input.IsActionJustPressed(input))
-            {
-                player.Position += new Vector2(30, 0);          
-                EmitSignal(nameof(ButtonReleasedSignal), player, true);
-            }
-            else
-            {
-                EmitSignal(nameof(ButtonReleasedSignal), player, false);
-                player.Position += new Vector2(-30, 0);
-                await Task.Delay(500);
-            }
+            player.Position += new Vector2(30, 0);
+            EmitSignal(nameof(ButtonReleasedSignal), player, true);
+
+            lastPressedState[player] = true;
+            canPressAgain[player] = false;
+        }
+
+        // **Detect wrong button press**
+        if (Input.IsAnythingPressed() && !isCorrectPressed && !lastPressedState[player] && canPressAgain[player])
+        {
+            GD.Print($"{player.Name} pressed the wrong button!");
+
+            player.Position -= new Vector2(30, 0); // Move player backward as penalty
+            EmitSignal(nameof(ButtonReleasedSignal), player, false);
+
+            lastPressedState[player] = true;
+            canPressAgain[player] = false;
+            await Task.Delay(1500); // Small delay to prevent spam
+            canPressAgain[player] = true;
+        }
+
+        // Handle key release
+        if (!isCorrectPressed && lastPressedState[player])
+        {
+            lastPressedState[player] = false;
+            await Task.Delay(200);
+            canPressAgain[player] = true;
         }
     }
+
+
+
     private void ButtonReleased(CharacterBody2D player, bool RightButton)
     {
         GD.Print($"{player.Name} pressed the {RightButton} button");
